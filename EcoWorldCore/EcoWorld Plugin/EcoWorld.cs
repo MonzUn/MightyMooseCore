33f5113df0e6fa13f6174db88a6d46d67a793b91
MonzUn <daniel-bengtsson@live.com>
2023-04-08 23:40:48 +0200

    Added base plugin and config

---
+++ EcoWorldCore/EcoWorld Plugin/EcoWorld.cs
@@ -0,0 +1,42 @@
+﻿using Eco.Core;
+using Eco.Core.Plugins;
+using Eco.Core.Plugins.Interfaces;
+using Eco.Core.Utils;
+using Eco.EW.Tools;
+
+namespace Eco.EW.Plugins
+{
+    [Priority(PriorityAttribute.VeryHigh)] // Need to start before any dependent plugins
+    public class EcoWorldCore : IModKitPlugin, IInitializablePlugin, IConfigurablePlugin
+    {
+        public static EcoWorldCore Obj { get { return PluginManager.GetPlugin<EcoWorldCore>(); } }
+        public ThreadSafeAction<object, string> ParamChanged { get; set; }
+        public string Status
+        {
+            get { return status; }
+            private set
+            {
+                Logger.Debug($"Plugin status changed from \"{status}\" to \"{value}\"");
+                status = value;
+            }
+        }
+        private string status = "Uninitialized";
+
+        private readonly PluginConfig<EcoWorldCoreConfig> config = new PluginConfig<EcoWorldCoreConfig>("EcoWorldCore");
+
+        public override string ToString() => "EcoWorld Core";
+        public string GetCategory() => "EcoWorld Mods";
+        public string GetStatus() => Status;
+        public IPluginConfig PluginConfig => config;
+        public EcoWorldCoreConfig ConfigData => config.Config;
+        public object GetEditObject() => config.Config;
+        public void OnEditObjectChanged(object o, string param) =>  ConfigData.OnConfigChanged(param);
+
+        public void Initialize(TimedTask timer)
+        {
+            Logger.RegisterLogger("DiscordLink", ConsoleColor.Green, ConfigData.LogLevel);
+            Status = "Initializing";
+            Status = "Running";
+        }
+    }
+}
---
+++ EcoWorldCore/EcoWorld Plugin/EcoWorldConfig.cs
@@ -0,0 +1,24 @@
+﻿using Eco.EW.Tools;
+using System.ComponentModel;
+
+namespace Eco.EW.Plugins
+{
+    public class EcoWorldCoreConfig
+    {
+        public static class DefaultValues
+        {
+            public static Logger.LogLevel LogLevel = Logger.LogLevel.Information;
+        }
+
+        public void OnConfigChanged(string param)
+        {
+            if (param == nameof(LogLevel))
+            {
+                Logger.SetConfiguredLogLevel(LogLevel);
+            }
+        }
+
+        [Description("Determines what message types will be printed to the server log. All message types below the selected one will be printed as well.")]
+        public Logger.LogLevel LogLevel { get; set; } = DefaultValues.LogLevel;
+    }
+}
