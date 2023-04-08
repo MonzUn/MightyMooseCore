e4d8e2b2e122934eb377988363caefb20858b38d
MonzUn <daniel-bengtsson@live.com>
2023-04-08 23:40:31 +0200

    Added version checker and peristance tools

---
+++ EcoWorldCore/Tools/VersionChecker.cs
@@ -0,0 +1,148 @@
+﻿using Eco.Core.Serialization;
+using Eco.ModKit;
+using Eco.Shared.Utils;
+using System.Diagnostics;
+using static Eco.EW.Utils.Console;
+
+namespace Eco.EW.Tools
+{
+    public static class VersionChecker
+    {
+        private static HttpClient _client = new();
+
+        private class ModIoRequestData
+        {
+            public int Status { get; set; }
+            public string? Profile_Url { get; set; }
+            public Dictionary<string, object>? ModFile { get; set; }
+        }
+
+        private class InstalledModData
+        {
+            public Version? Version;
+        }
+
+        private class ModIoData
+        {
+            public Version? Version;
+            public string? ModPageUrl;
+        }
+
+        public static async void CheckVersion(string modName, string modIoModId, string modIoApiKey)
+        {
+            ModIoData? modIoData = await GetModIoData(modIoModId, modIoApiKey);
+            InstalledModData? installedData = GetInstalledModData(modName);
+            if (modIoData == null || installedData == null || modIoData.Version == null || installedData.Version == null)
+            {
+                return;
+            }
+
+            ConsoleOutputComponent[] components;
+            if (modIoData.Version == installedData.Version)
+            {
+                components = new[]
+                {
+                    new ConsoleOutputComponent($"{modName} ", ConsoleColor.Green),
+                    new ConsoleOutputComponent($"- installed version ", ConsoleColor.Yellow),
+                    new ConsoleOutputComponent($"({installedData.Version}) - Up to date!", ConsoleColor.Green),
+                };
+
+            }
+            else if (modIoData.Version > installedData.Version)
+            {
+                components = new[]
+                {
+                    new ConsoleOutputComponent($"{modName} ", ConsoleColor.Green),
+                    new ConsoleOutputComponent($"- installed version ", ConsoleColor.Yellow),
+                    new ConsoleOutputComponent($"({installedData.Version}) - Outdated!\n", ConsoleColor.Red),
+                    new ConsoleOutputComponent($"Please download version ", ConsoleColor.Yellow),
+                    new ConsoleOutputComponent($"({modIoData.Version}) ", ConsoleColor.Cyan),
+                    new ConsoleOutputComponent($"from ", ConsoleColor.Yellow),
+                    new ConsoleOutputComponent($"{modIoData.ModPageUrl}", ConsoleColor.Gray),
+                };
+            }
+            else
+            {
+                components = new[]
+                {
+                    new ConsoleOutputComponent($"{modName} ", ConsoleColor.Green),
+                    new ConsoleOutputComponent($"- installed version ", ConsoleColor.Yellow),
+                    new ConsoleOutputComponent($"({installedData.Version}) - Unreleased", ConsoleColor.Cyan),
+                };
+            }
+            PrintConsoleColored(components);
+        }
+
+        private static async Task<ModIoData?> GetModIoData(string modIoModId, string modIoApiKey)
+        {
+            string? modIoJsonResult = string.Empty;
+            try
+            {
+                modIoJsonResult = await _client.GetStringAsync($"https://api.mod.io/v1/games/6/mods/{modIoModId}?api_key={modIoApiKey}");
+            }
+            catch (Exception e)
+            {
+                Logger.Exception($"Failed to fetch mod metadata for mod with ID {modIoModId} from ModIO", e);
+                return null;
+            }
+
+            if (modIoJsonResult == null)
+            {
+                Logger.Error($"ModIO mod metadata for mod with ID {modIoModId} was null");
+                return null;
+            }
+
+            ModIoRequestData? modIoRequestResult = SerializationUtils.DeserializeJson<ModIoRequestData>(modIoJsonResult);
+            if (modIoRequestResult == null)
+            {
+                Logger.Error($"Failed to parse ModIO mod metadata for mod with ID {modIoModId}");
+                return null;
+            }
+
+            if (modIoRequestResult.Status != 1)
+            {
+                Logger.Error($"Mod metadate for mod with ID {modIoModId} returned invalid status");
+                return null;
+            }
+
+            ModIoData modIoData = new ModIoData();
+            modIoData.ModPageUrl = modIoRequestResult.Profile_Url;
+
+            string? versionStr = (string?)modIoRequestResult.ModFile["version"];
+            try
+            {
+                modIoData.Version = Version.Parse(versionStr);
+            }
+            catch
+            {
+                Logger.Warning($"Failed to parse ModIO mod version string for mod with ID {modIoModId}");
+            }
+
+            return modIoData;
+        }
+
+        private static InstalledModData? GetInstalledModData(string productName)
+        {
+            Version version = null;
+            string[] assemblies = Directory.GetFiles(ModKitPlugin.ModDirectory, "*.dll", SearchOption.AllDirectories);
+            foreach (string assembly in assemblies)
+            {
+                FileVersionInfo? assemblyData = FileVersionInfo.GetVersionInfo(assembly);
+                if (assemblyData != null && assemblyData.FileVersion != null && assemblyData.ProductName.EqualsCaseInsensitive(productName))
+                {
+                    try
+                    {
+                        version = Version.Parse(assemblyData.FileVersion);
+                    }
+                    catch
+                    {
+                        Logger.Warning($"Failed to parse installed mod version string for {productName}");
+                        return null;
+                    }
+                    break;
+                }
+            }
+            return new InstalledModData { Version = version };
+        }
+    }
+}
---
+++ EcoWorldCore/Utils/Persistance.cs
@@ -0,0 +1,106 @@
+﻿using Eco.Core.Serialization;
+using Eco.EW.Tools;
+
+namespace Eco.EW.Utils
+{
+    public static class Persistance
+    {
+        public static bool WriteJsonToFile<T>(T data, string directoryPath, string nameAndExtension)
+        {
+            // Serialize data to JSON
+            string json = SerializationUtils.SerializeJson(data);
+            if(string.IsNullOrWhiteSpace(json))
+            {
+                Logger.Error($"Failed to serialize data for storage file \"{nameAndExtension}\"");
+                return false;
+            }
+
+            // Ensure directory exists
+            try
+            {
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+            }
+            catch (Exception e)
+            {
+                Logger.Exception($"Failed to create directory \"{directoryPath}\" while writing storage file \"{nameAndExtension}\"", e);
+                return false;
+            }
+
+            // Parse path
+            string path;
+            try
+            {
+                path = Path.Combine(directoryPath, nameAndExtension);
+            }
+            catch(Exception e)
+            {
+                Logger.Exception($"Failed to parse path for writing storage file. Directory = \"{directoryPath}\" | Filename = \"{nameAndExtension}\"", e);
+                return false;
+            }
+            
+            // Write JSON to file
+            try
+            {
+                StreamWriter writer = new StreamWriter(path);
+                writer.Write(data);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.Exception($"Failed to write JSON storage data to \"{path}\"", e);
+                return false;
+            }
+        }
+
+        public static T? ReadJsonFromFile<T>(string directoryPath, string nameAndExtension)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Logger.Silent($"Failed to find directory \"{directoryPath}\" for reading storage file \"{nameAndExtension}\"");
+                return default;
+            }
+
+            // Parse path
+            string path;
+            try
+            {
+                path = Path.Combine(directoryPath, nameAndExtension);
+            }
+            catch (Exception e)
+            {
+                Logger.Exception($"Failed to parse path for reading storage file. Directory = \"{directoryPath}\" | Filename = \"{nameAndExtension}\"", e);
+                return default;
+            }
+
+
+            if (!File.Exists(path))
+            {
+                Logger.Silent($"Failed to find file \"{path}\" for reading storage data");
+                return default;
+            }
+
+            string jsonStr;
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                jsonStr = reader.ReadToEnd();
+                reader.Close();
+                reader.Dispose();
+            }
+            catch(Exception e)
+            {
+                Logger.Exception($"Failed to read storage data from \"{path}\"", e);
+                return default;
+            }
+
+            T result = SerializationUtils.DeserializeJson<T>(jsonStr);
+            if(result == null)
+            {
+                Logger.Error($"Failed to parse JSON storage data from \"{path}\"");
+            }
+
+            return result;
+        }
+    }
+}
