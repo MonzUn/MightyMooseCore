6becf31cbb5f9974349083371a3525480972d051
MonzUn <daniel-bengtsson@live.com>
2023-04-08 23:40:16 +0200

    Added logger and console printing

---
+++ EcoWorldCore/Tools/Logger.cs
@@ -0,0 +1,146 @@
+﻿using Eco.Core.Utils.Logging;
+using Eco.Shared.Localization;
+using System.Reflection;
+using static Eco.EW.Utils.Console;
+using static Eco.Shared.Utils.ILogWriter;
+
+namespace Eco.EW.Tools
+{
+    public static class Logger
+    {
+        public enum LogLevel
+        {
+            DebugVerbose, // Ignored unless the configured log level is also DebugVerbose
+            Debug,
+            Warning,
+            Information,
+            Error,
+            Silent, // Prints to the log file only
+        }
+
+        private static ConsoleColor[] LogLevelColors = new ConsoleColor[]
+        {
+            ConsoleColor.Gray,          // DebugVerbose
+            ConsoleColor.Gray,          // Debug
+            ConsoleColor.DarkYellow,    // Warning
+            ConsoleColor.White,         // Information
+            ConsoleColor.DarkRed,       // Error
+        };
+
+        public struct LogData
+        {
+            public string Tag;
+            public ConsoleColor TagColor;
+            public LogLevel ConfiguredLevel;
+            public NLogWriter Log;
+        }
+
+        private static Dictionary<Assembly, LogData> Loggers = new();
+
+        public static bool RegisterLogger(string tag, ConsoleColor tagColor, LogLevel configuredLevel)
+        {
+            Assembly caller = Assembly.GetCallingAssembly();
+            if (Loggers.ContainsKey(caller))
+            {
+                return false;
+            }
+
+            LogData data = new LogData
+            {
+                Tag = tag,
+                TagColor = tagColor,
+                ConfiguredLevel = configuredLevel,
+                Log = NLogManager.GetLogWriter(tag),
+            };
+
+            Loggers.Add(caller, data);
+            return true;
+        }
+
+        public static bool SetConfiguredLogLevel(LogLevel level)
+        {
+            if (Loggers.TryGetValue(Assembly.GetCallingAssembly(), out LogData logData))
+            {
+                logData.ConfiguredLevel = level;
+                return true;
+            }
+            return false;
+        }
+
+        public static void DebugVerbose(string message) => Write(message, LogLevel.DebugVerbose, Assembly.GetCallingAssembly());
+
+        public static void Debug(string message) => Write(message, LogLevel.Debug, Assembly.GetCallingAssembly());
+
+        public static void Warning(string message) => Write(message, LogLevel.Warning, Assembly.GetCallingAssembly());
+
+        public static void Info(string message) => Write(message, LogLevel.Information, Assembly.GetCallingAssembly());
+
+        public static void Error(string message) => Write(message, LogLevel.Error, Assembly.GetCallingAssembly());
+
+        public static void Exception(string message, Exception exception) => Write(message, LogLevel.Error, Assembly.GetCallingAssembly(), exception);
+
+        public static void Silent(string message) => Write(message, LogLevel.Silent, Assembly.GetCallingAssembly());
+
+        private static void Write(string message, LogLevel level, Assembly caller, Exception? exception = null)
+        {
+            if (Loggers.TryGetValue(caller, out LogData logData))
+            {
+                switch (level)
+                {
+                    case LogLevel.DebugVerbose:
+                        if (logData.ConfiguredLevel <= LogLevel.DebugVerbose)
+                        {
+                            PrintToConsole(message, LogLevel.DebugVerbose, logData);
+                            logData.Log.Debug(FormatLogMessage(message)); // Verbose debug log messages are only written to the log file if enabled via configuration
+                        }
+                        break;
+
+                    case LogLevel.Debug:
+                        if (logData.ConfiguredLevel <= LogLevel.Debug)
+                            PrintToConsole(message, LogLevel.Debug, logData);
+                        logData.Log.Debug(FormatLogMessage(message));
+                        break;
+
+                    case LogLevel.Warning:
+                        if (logData.ConfiguredLevel <= LogLevel.Warning)
+                            PrintToConsole(message, LogLevel.Warning, logData);
+                        logData.Log.WriteWarning(FormatLogMessage(message));
+                        break;
+
+                    case LogLevel.Information:
+                        if (logData.ConfiguredLevel <= LogLevel.Information)
+                            PrintToConsole(message, LogLevel.Information, logData);
+                        logData.Log.Write(FormatLogMessage(message));
+                        break;
+
+                    case LogLevel.Error:
+                        if (logData.ConfiguredLevel <= LogLevel.Error)
+                            PrintToConsole(message, LogLevel.Error, logData);
+
+                        ErrorInfo errorInfo = new ErrorInfo(FormatLogMessage(message), exception);
+                        logData.Log.WriteError(ref errorInfo, stripTagsForConsole: true);
+                        break;
+
+                    case LogLevel.Silent:
+                        logData.Log.Write(FormatLogMessage(message));
+                        break;
+                }
+            }
+        }
+
+        private static string FormatLogMessage(string message) => ($"[{DateTime.Now:hh:mm:ss}] {message}");
+
+        private static void PrintToConsole(string message, LogLevel level, LogData logData)
+        {
+            if (logData.ConfiguredLevel == LogLevel.Silent)
+                return;
+
+            ConsoleOutputComponent[] components = new[]
+            {
+                    new ConsoleOutputComponent(Localizer.DoStr($"[{logData.Tag}] "), logData.TagColor),
+                    new ConsoleOutputComponent(Localizer.DoStr($"[{message}] "), LogLevelColors[(int)level]),
+                };
+            PrintConsoleColored(components);
+        }
+    }
+}
---
+++ EcoWorldCore/Utils/Console.cs
@@ -0,0 +1,46 @@
+﻿using Eco.Core.Utils.IO;
+using Eco.Core.Utils.Logging;
+using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+
+namespace Eco.EW.Utils
+{
+    public static class Console
+    {
+        public struct ConsoleOutputComponent
+        {
+            public ConsoleOutputComponent(string message, ConsoleColor color) { this.Message = message; this.Color = color; }
+
+            public string Message;
+            public ConsoleColor Color;
+        }
+
+        public static void PrintConsoleColored(ConsoleOutputComponent[] outputComponents)
+        {
+            string? context = outputComponents.SelectNonNull(c => c.Message).ToString();
+            if (context == null)
+                return;
+
+            string timestamp = Localizer.DoStr($"[{DateTime.Now:hh:mm:ss}] ");
+            context = timestamp + context;
+
+            lock (ConsoleLogWriter.Instance)
+            {
+                ConsoleSynchronizationContext.Instance.Post(context =>
+                {
+                    System.Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    System.Console.Write(timestamp);
+
+                    foreach (var component in outputComponents)
+                    {
+                        System.Console.ForegroundColor = component.Color;
+                        System.Console.Write(Localizer.DoStr(component.Message));
+                    }
+
+                    System.Console.Write("\n");
+                    System.Console.ResetColor();
+                }, context);
+            }
+        }
+    }
+}
