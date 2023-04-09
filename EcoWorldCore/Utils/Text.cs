3d9bd2b83d9675e32c05649baaf24617d8059176
MonzUn <daniel-bengtsson@live.com>
2023-04-09 02:29:28 +0200

    Added TextColor utils

---
+++ EcoWorldCore/Utils/Text.cs
@@ -0,0 +1,9 @@
+﻿namespace Eco.EW.Utils
+{
+    public class TextColor
+    {
+        public static string Green(string msg) => $"<color=green>{msg}</color>";
+        public static string Yellow(string msg) => $"<color=yellow>{msg}</color>";
+        public static string Red(string msg) => $"<color=red>{msg}</color>";
+    }
+}
