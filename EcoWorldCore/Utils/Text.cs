4f06976ece860ca8d70ffa025be422c26c96a203
MonzUn <daniel-bengtsson@live.com>
2023-12-17 22:11:32 +0100

    Namespace changes for Text utils

--- EcoWorldCore/Utils/Text.cs
+++ EcoWorldCore/Utils/Text.cs
@@ -1,9 +1,12 @@
-﻿namespace Eco.EW.Utils
+namespace Eco.EW.Utils
 {
-    public class TextColor
+    public static class Text
     {
-        public static string Green(string msg) => $"<color=green>{msg}</color>";
-        public static string Yellow(string msg) => $"<color=yellow>{msg}</color>";
-        public static string Red(string msg) => $"<color=red>{msg}</color>";
+        public class Color
+        {
+            public static string Green(string msg) => $"<color=green>{msg}</color>";
+            public static string Yellow(string msg) => $"<color=yellow>{msg}</color>";
+            public static string Red(string msg) => $"<color=red>{msg}</color>";
+        }
     }
 }
