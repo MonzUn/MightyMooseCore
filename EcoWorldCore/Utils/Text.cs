63fdba3796accc882c2f4a1856fd93572dfb89da
MonzUn <daniel-bengtsson@live.com>
2023-12-17 22:11:44 +0100

    Added Utils.Text.GetEnumDescription

--- EcoWorldCore/Utils/Text.cs
+++ EcoWorldCore/Utils/Text.cs
@@ -1,3 +1,6 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
 namespace Eco.EW.Utils
 {
     public static class Text
@@ -7,6 +10,13 @@ namespace Eco.EW.Utils
             public static string Green(string msg) => $"<color=green>{msg}</color>";
             public static string Yellow(string msg) => $"<color=yellow>{msg}</color>";
             public static string Red(string msg) => $"<color=red>{msg}</color>";
+        }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
         }
     }
 }
