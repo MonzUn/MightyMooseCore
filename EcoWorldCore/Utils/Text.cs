1577c2c53f41a04f0439d5ebde7d953f4ad005f7
MonzUn <daniel-bengtsson@live.com>
2023-12-20 22:33:51 +0100

    Added Utils.Text.StripTags

--- EcoWorldCore/Utils/Text.cs
+++ EcoWorldCore/Utils/Text.cs
@@ -1,15 +1,27 @@
 ﻿using System.ComponentModel;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Eco.EW.Utils
 {
     public static class Text
     {
+        // Eco tag matching regex: Match all characters that are used to create HTML style tags
+        private static readonly Regex HTMLTagRegex = new Regex("<[^>]*>");
+
         public class Color
         {
             public static string Green(string msg) => $"<color=green>{msg}</color>";
             public static string Yellow(string msg) => $"<color=yellow>{msg}</color>";
             public static string Red(string msg) => $"<color=red>{msg}</color>";
+        }
+
+        public static string StripTags(string toStrip)
+        {
+            if (toStrip == null)
+                return string.Empty;
+
+            return HTMLTagRegex.Replace(toStrip, string.Empty);
         }
 
         public static string GetEnumDescription(Enum value)
