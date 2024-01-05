e4d6dd45a3f70943d22c261bc506b2899a859340
MonzUn <daniel-bengtsson@live.com>
2024-01-05 12:39:16 +0100

    Migrated Lookups, Constants and Extensions from DiscordLink

---
+++ MightyMooseCore/Utils/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace Eco.Moose.Utils
+{
+    public static class Constants
+    {
+        public const int SECONDS_PER_MINUTE = 60;
+        public const int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+        public const int SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
+        public const int SECONDS_PER_WEEK = SECONDS_PER_DAY * 7;
+
+        public const int MILLISECONDS_PER_MINUTE = SECONDS_PER_MINUTE * 1000;
+        public const int MILLISECONDS_PER_HOUR = MILLISECONDS_PER_MINUTE * 60;
+        public const int MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24;
+        public const int MILLISECONDS_PER_WEEK = MILLISECONDS_PER_DAY * 7;
+
+        public const int ECO_PLOT_SIZE_M2 = 5 * 5;
+    }
+}
