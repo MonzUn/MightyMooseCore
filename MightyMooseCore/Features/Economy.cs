108cf452e8c3204bfb663b95a4624390a96a1fdf
MonzUn <daniel-bengtsson@live.com>
2025-06-26 17:00:39 +0200

    Added CalculateActiveCurrencyCirculation

---
+++ MightyMooseCore/Features/Economy.cs
@@ -0,0 +1,24 @@
+﻿
+using Eco.Gameplay.Economy;
+
+namespace Eco.Moose.Features
+{
+    public static class Economy
+    {
+        public static float CalculateActiveCurrencyCirculation(Currency currency, bool includeTreasuaries = false)
+        {
+            float circulationAmount = 0.0f;
+            foreach ((BankAccount account, CurrencyHolding holding) in BankAccountManager.Obj.GetAccountsForCurrency(currency))
+            {
+                if (!includeTreasuaries && account.Settlement != null && account == account.Settlement.TreasuryBankAccount)
+                    continue; // Ignore treasury accounts
+
+                if(account.DualPermissions.AllUsers.Any(user => user.IsActive))
+                {
+                    circulationAmount += holding.Val;
+                }
+            }
+            return circulationAmount;
+        }
+    }
+}
