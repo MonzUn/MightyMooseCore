71eaf07bce22b44ddae7cddcdcbc15434c6adbf6
MonzUn <daniel-bengtsson@live.com>
2025-07-06 12:59:08 +0200

    Added component lookups

--- MightyMooseCore/Utils/Lookups.cs
+++ MightyMooseCore/Utils/Lookups.cs
@@ -20,7 +20,9 @@ using Eco.Gameplay.Skills;
 using Eco.Gameplay.Systems.Messaging.Chat.Channels;
 using Eco.Moose.Data;
 using Eco.Plugins.Networking;
+using Eco.Shared.IoC;
 using Eco.Shared.Items;
+using Eco.Shared.Math;
 using Eco.Shared.Time;
 using Eco.Shared.Utils;
 using Eco.Simulation.Time;
@@ -55,15 +57,19 @@ namespace Eco.Moose.Utils.Lookups
         public static User OnlineUserByNameId(string userNameOrId) => int.TryParse(userNameOrId, out int id) ? OnlineUserById(id) : OnlineUserByName(userNameOrId);
         public static User OnlineUserByStrangeOrStrangeID(string strangeID, string steamID) => OnlineUsers.FirstOrDefault(user => user.SteamId.Equals(steamID) || user.StrangeId.Equals(strangeID));
 
-        public static IEnumerable<StoreComponent> StoresComponents => WorldObjectUtil.AllObjsWithComponent<StoreComponent>().Where(store => store.Parent.Enabled && store.Owners != null);
         public static IEnumerable<WorldObject> WorldObjects => ServiceHolder<IWorldObjectManager>.Obj.All;
         public static IEnumerable<WorldObject> WorldObjectsByComponent<T>() => WorldObjects.Where(worldObj => worldObj.HasComponent<T>());
         public static IEnumerable<WorldObject> WorldObjectsByTag(string tag) => WorldObjects.Where(worldObj => worldObj.TagNames().Contains(tag));
         public static IEnumerable<WorldObject> WorldObjectsWithinRange(Vector2 origin, float range) => ServiceHolder<IWorldObjectManager>.Obj.GetObjectsWithin(origin, range);
         public static IEnumerable<WorldObject> WorldObjectsWithinRange(WrappedPosition3 origin, float range) => ServiceHolder<IWorldObjectManager>.Obj.GetObjectsWithin(origin, range);
         public static WorldObject WorldObjectById(Guid id) => ServiceHolder<IWorldObjectManager>.Obj.GetFromID(id);
+
+        public static IEnumerable<T> AllComponentsOfType<T>() where T : WorldObjectComponent => WorldObjects.SelectNonNull(worldObj => worldObj.GetComponent<T>());
+        public static IEnumerable<CustomTextComponent> CustomTexts => AllComponentsOfType<CustomTextComponent>();
         public static IEnumerable<RepairBountyComponent> RepairBounties => AllComponentsOfType<RepairBountyComponent>().Where(repairBounty => repairBounty.Enabled);
         public static IEnumerable<RepairBountyComponent> ClaimableRepairBounties => RepairBounties.Where(bounty => bounty.ShowRepairBounty());
+
+        public static IEnumerable<StoreComponent> StoresComponents => AllComponentsOfType<StoreComponent>().Where(store => store.Parent.Enabled && store.Owners != null);
 
         public static IEnumerable<Settlement> Settlements => Registrars.Get<Settlement>().NonNull();
         public static IEnumerable<Settlement> ActiveSettlements => Settlements.Where(settlement => settlement.IsActive);
